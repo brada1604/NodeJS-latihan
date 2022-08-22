817069b9c1c5a5a7cebeb8e3ca5736ae69a6a213
brada1604 <bagusnugroho104@gmail.com>
2022-08-22 15:52:13 +0700

    commit file

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 // SECTION 3
 // LESSON 3
-const checkUtils = require('./src/utils.js')
-checkUtils()
+// const checkUtils = require('./src/utils.js')
+// checkUtils()
+
+const command = process.argv[2]
+if (command === 'add') {
+console.log('Adding note!')
+} else if (command === 'remove') {
+console.log('Removing note!')
+}
