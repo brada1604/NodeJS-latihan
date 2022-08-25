6054073971650c6754897378e70836269cc95be8
brada1604 <bagusnugroho104@gmail.com>
2022-08-25 15:05:27 +0700

    json

--- app.js
+++ app.js
@@ -10,8 +10,8 @@
 // console.log('Removing note!')
 // }
 
-const yargs = require('yargs')
-yargs.version('1.1.0')
+// const yargs = require('yargs')
+// yargs.version('1.1.0')
 // yargs.command({
 //  command: 'add',
 //  describe: 'Add a new note',
@@ -20,24 +20,36 @@ yargs.version('1.1.0')
 //  }
 // })
 
-yargs.command({
-	command: 'add',
-	describe: 'Add a new note',
-	builder: {
-		title: {
-			describe: 'Note title',
-			demandOption: true,
-			type: 'string'
-		},
-		body: {
-			describe: 'Note body',
-			demandOption: true,
-			type: 'string'
-		}
-	},
-	handler: function (argv) {
-		console.log('Title: ' + argv.title)
-		console.log('Body: ' + argv.body)
-	}
-})
-console.log(yargs.argv)
+// const yargs = require('yargs')
+// yargs.version('1.1.0')
+// yargs.command({
+// 	command: 'add',
+// 	describe: 'Add a new note',
+// 	builder: {
+// 		title: {
+// 			describe: 'Note title',
+// 			demandOption: true,
+// 			type: 'string'
+// 		},
+// 		body: {
+// 			describe: 'Note body',
+// 			demandOption: true,
+// 			type: 'string'
+// 		}
+// 	},
+// 	handler: function (argv) {
+// 		console.log('Title: ' + argv.title)
+// 		console.log('Body: ' + argv.body)
+// 	}
+// })
+// console.log(yargs.argv)
+
+const book = {
+ title: 'Ego is the Enemy',
+ author: 'Ryan Holiday'
+}
+// Covert JavaScript object into JSON string
+const bookJSON = JSON.stringify(book)
+// Covert JSON string into object
+const bookObject = JSON.parse(bookJSON)
+console.log(bookObject.title) // Print: Ego is the Enemy
