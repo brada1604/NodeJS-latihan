7ed3f0469cab3104725ce8557c58f87de9c8db00
brada1604 <bagusnugroho104@gmail.com>
2022-09-01 17:21:14 +0700

    Printing in Color

---
+++ Section_3/lesson5.js
@@ -0,0 +1,6 @@
+// IMPORT LIBRARY CHALK
+import chalk from "chalk";
+
+console.log(chalk.blue("Hello") + " World" + chalk.red("!"));
+console.log(chalk.blue.bgRed.bold("Perkenalkan"));
+console.log(chalk.bold("Nama saya Bagus Nugroho"));
