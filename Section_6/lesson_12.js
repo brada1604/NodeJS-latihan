4cbbf559cb137341ae1ecd0511c394bd86e08176
brada1604 <bagusnugroho104@gmail.com>
2022-09-01 23:53:20 +0700

    ES6 Aside: Object Property Shorthand and  Destructuring

--- Section_6/lesson_12.js
+++ Section_6/lesson_12.js
@@ -1,1 +1,26 @@
-lesson_12
+const name = "Andrew";
+
+const userAge = 27;
+
+const user = {
+	name,
+	age: userAge,
+	location: "Philadelphia",
+};
+const { age, location: address } = user;
+
+console.log(age);
+console.log(address);
+
+
+const product = {
+	label: "Red notebook",
+	price: 3,
+	stock: 201,
+	salePrice: undefined,
+	rating: 4.2,
+};
+
+const transaction = (type, { label, stock }) => [console.log(type, label, stock)];
+
+transaction("order", product);
