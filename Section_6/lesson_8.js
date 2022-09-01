c3490820264957510873d7be3c9ce3ee1482c1f0
brada1604 <bagusnugroho104@gmail.com>
2022-09-01 23:36:21 +0700

    The Callback Function

--- Section_6/lesson_8.js
+++ Section_6/lesson_8.js
@@ -1,1 +1,12 @@
-lesson_8
+const geocode = (address, callback) => {
+	setTimeout(() => {
+		const data = {
+			latitude: 0,
+			longitude: 0,
+		};
+		callback(data);
+	}, 2000);
+};
+geocode("Philadelphia", (data) => {
+	console.log(data);
+});
