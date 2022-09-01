d5f092a3fad70302349a0fd3152209b6c675882a
brada1604 <bagusnugroho104@gmail.com>
2022-09-02 00:03:12 +0700

    Destructuring and Property Shorthand Challenge

--- Section_6/lesson_13.js
+++ Section_6/lesson_13.js
@@ -1,1 +1,17 @@
-lesson_13
+const name = "Bagus Nugroho";
+const email = "bagus@gmail.com";
+const password = "banu";
+const address = "Ciamis";
+
+const account = {
+	name,
+	email,
+	password,
+	address,
+};
+
+const login = ({ email, password, name }) => {
+	console.log(name + " success login");
+};
+
+login(account);
