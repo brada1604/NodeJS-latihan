55a3fa6886b7f44c2e49ebc84b895db5f0c15adc
brada1604 <bagusnugroho104@gmail.com>
2022-09-01 23:18:28 +0700

    Customizing HTTP Requests

--- Section_6/lesson_5.js
+++ Section_6/lesson_5.js
@@ -1,1 +1,12 @@
-lesson_5
+const request = require('request')
+
+const url = 'https://data.covid19.go.id/public/api/prov.json'
+
+request({ url: url }, (error, response) => {
+	// Parse the response body from JSON string into JavaScript object
+	const data = JSON.parse(response.body)
+
+	// Will print the current temperature to the console
+	console.log("Jumlah Kasus Covid Indonesia : " + data.list_data[0].jumlah_kasus)
+	console.log("Jumlah Kasus Sembuh Indonesia : " + data.list_data[0].jumlah_sembuh)
+})
