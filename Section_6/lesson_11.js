b6f60d51bbd3011cf62a8c11a3d481c3648e30cc
brada1604 <bagusnugroho104@gmail.com>
2022-09-01 23:49:25 +0700

    Callback Chaining

--- Section_6/lesson_11.js
+++ Section_6/lesson_11.js
@@ -1,1 +1,16 @@
-lesson_11
+const weather = require("./utils/weather");
+
+const geocode = require("./utils/geocode");
+
+geocode("Ciamis", (error, data) => {
+	if (error) {
+		return console.log(error);
+	}
+	weather(data.location, (error, forecastData) => {
+		if (error) {
+			return console.log(error);
+		}
+		console.log(data.location);
+		console.log(forecastData);
+	});
+});
